4601628c1040f161782577dc0297e3557ac3ef8f
Lucas Reicher <42223412+lreicher@users.noreply.github.com>
2021-06-11 22:10:24 -0700

    Cleaned Up
    New Usage
    luhn.py -c type (generate number)
    luhn.py -v number (validated number)

--- luhn.py
+++ luhn.py
@@ -7,20 +7,17 @@ def random_with_N_digits(n):
     return random.randint(range_start, range_end)
 
 def get_random_card_by_type(type):
+    sum_digits = ""
     if type == "visa":
-        sum_digits = "4"
-        sum_digits += str(random_with_N_digits(14))
+        sum_digits = "4" + str(random_with_N_digits(14))
     elif type == "mastercard":
-        sum_digits = str(random.randint(51,55))
-        sum_digits += str(random_with_N_digits(13))
+        sum_digits = str(random.randint(51,55)) + str(random_with_N_digits(13))
     elif type == "amex":
-        sum_digits = str(random.choice([34, 37]))
-        sum_digits += str(random_with_N_digits(12))
+        sum_digits = str(random.choice([34, 37])) + str(random_with_N_digits(12))
     return sum_digits
 
-def check_Luhn(number):
-    length = len(number)
-    total = int(number[length-1])
+def sum_digits(number, length):
+    total = 0
     parity = length % 2
     for i in range(0, length-1):
         digit = int(number[i])
@@ -29,35 +26,30 @@ def check_Luhn(number):
         if digit > 9:
             digit -= 9
         total += digit
+    return total
+
+def check_Luhn(number):
+    length = len(number)
+    total = int(number[length-1]) + sum_digits(number, length)
     return (total % 10) == 0
 
 def generate_Luhn(type):
     sum_digits = get_random_card_by_type(type)
     return add_check_sum(sum_digits)
 
-def add_check_sum(sum_digits):
-    length = len(sum_digits)+1
-    total = 0
-    even = True
-    for i in range(length-2, -1, -1):
-        if even:
-            num = str(int(sum_digits[i])*2)
-            if len(num) == 2:
-                num = int(num[0]) + int(num[1])
-            total += int(num)
-            even = not even
-        else:
-            total += int(sum_digits[i])
-            even = not even
+def add_check_sum(number):
+    total = sum_digits(number, len(number)+1)
     check_digit = (total * 9) % 10
-    sum_digits += str(check_digit)
-    return sum_digits
+    return number + str(check_digit)
 
 if __name__ == '__main__':
     argc = len(sys.argv)
-    if argc == 2:
-        gen_luhn = generate_Luhn(sys.argv[1])
-        print(gen_luhn)
-        print(check_Luhn(gen_luhn))
+    if argc == 3:
+        if sys.argv[1] == '-v':
+            print(check_Luhn(sys.argv[2]))
+        elif sys.argv[1] == '-c':
+            print(generate_Luhn(sys.argv[2]))
+        else:
+            print("usage: luhn.py type")
     else:
         print("usage: luhn.py type")
